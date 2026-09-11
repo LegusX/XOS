d05008d9f9a31c81ea4208f4ea51ca911034b19e
Logan Henrie <loghenrie@gmail.com>
2026-09-10 21:44:21 -0400

    idk anymore

--- build_files/build.sh
+++ build_files/build.sh
@@ -30,8 +30,8 @@ dnf remove -y \
     waybar \
 
 ### Enable various system units
-systemctl --user add-wants niri.service dms
-systemctl enable greetd.service power-pruninstallofiles-daemon.service
+systemctl --root=/ --user add-wants niri.service dms
+systemctl --root=/ enable greetd.service power-pruninstallofiles-daemon.service
 
 ### Create default account
 useradd --root=/ \
@@ -40,14 +40,3 @@ useradd --root=/ \
         --groups wheel,greeter,video,audio,input \
         --shell /usr/bin/zsh \
         logan
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
