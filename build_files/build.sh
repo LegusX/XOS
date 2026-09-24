35a48f093fb9ae1f659fec55934a0264c86daf6f
Logan Henrie <loghenrie@gmail.com>
2026-09-24 09:16:35 -0400

    skip unavailable

--- build_files/build.sh
+++ build_files/build.sh
@@ -18,9 +18,9 @@ dnf copr enable -y avengemedia/danklinux
 dnf copr enable -y scottames/ghostty
 dnf copr enable -y scottames/awww
 
-# dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release terra-gpg-keys
+dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release terra-gpg-keys
 
-dnf install -y \
+dnf install -y --skip-unavailable \
     dms \
     ghostty \
     dms-greeter \
